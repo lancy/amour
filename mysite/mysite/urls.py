10b07e0e226c7f6667c176c24699eba6cbbca80b
lancy <lancy1014@gmail.com>
2012-10-31 23:06:47 +0800

    hello world

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,6 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^amour/', include("amour.urls")),
-    url(r'^polls/', include("polls.urls")),
+    # url(r'^polls/', include("polls.urls")),
     # url(r'^admin/', include(admin.site.urls)),
 )
